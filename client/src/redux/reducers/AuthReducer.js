defa0973f073a593630628ec91bc709f0a449d27
Dmitry Dutin <dmitrydutin@gmail.com>
2021-01-17 13:59:38 +0300

    fix: edit some bugs

--- client/src/redux/reducers/AuthReducer.js
+++ client/src/redux/reducers/AuthReducer.js
@@ -1,8 +1,39 @@
-const initialState = {};
+import { AuthAPI } from '../../api/api';
+
+const SET_USER_DATA = 'SET_USER_DATA';
+
+const initialState = {
+    isAuth: false,
+    role: null,
+};
 
 export const AuthReducer = (state = initialState, action) => {
     switch (action.type) {
+        case SET_USER_DATA:
+            return {
+                ...state,
+                ...action.payload,
+            };
         default:
             return state;
     }
+};
+
+const setUserDataAction = (isAuth, role) => {
+    return {
+        type: SET_USER_DATA,
+        payload: { isAuth, role },
+    };
+};
+
+export const login = (login, password, setSubmitting) => {
+    return async (dispatch) => {
+        const response = await AuthAPI.login(login, password);
+        setSubmitting(false);
+
+        if (response.status === 200) {
+            const { isAuth, role } = response.data;
+            dispatch(setUserDataAction(isAuth, role));
+        }
+    };
 };
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -6,11 +6,12 @@ router.post('/login', (req, res) => {
     const { login, password } = req.body;
 
     pool.getConnection((err, connection) => {
-        if (err) {
-            return res.status(500).json({ message: err.message, err });
-        }
+        if (err) return res.status(500).json({ message: err.message, err });
 
-        const query = 'SELECT * FROM users where login=? AND password=?';
+        const query = `
+            SELECT users.login, users.password, role.value AS role FROM users
+            INNER JOIN role ON users.role=role.id WHERE login=? AND password=?;
+        `;
 
         connection.query(query, [login, password], (error, results) => {
             connection.release();
@@ -19,7 +20,11 @@ router.post('/login', (req, res) => {
                 return res.status(500).json({ message: error.message, error });
             }
 
-            return res.json(results);
+            if (results.length === 0) {
+                return res.json({ isAuth: false, role: null });
+            }
+
+            return res.json({ isAuth: true, role: results[0].role });
         });
     });
 });
