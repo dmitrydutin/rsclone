fa68d2934eb29449508df35d5aa0c9a6a98aff7e
Dmitry Dutin <dmitrydutin@gmail.com>
2021-01-26 18:23:07 +0300

    feat: add logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,45 @@
+import { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { initializeApp } from './redux/reducers/AppReducer';
 import { Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { LinearProgress } from '@material-ui/core';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import Join from './components/Join/Join';
 import Error from './components/Error/Error';
 
-const App = () => {
+const App = (props) => {
+    const { token } = props;
+
+    useEffect(() => {
+        props.initializeApp(token);
+    }, []);
+
     return (
         <>
             <CssBaseline />
-            <Header />
 
-            <Switch>
-                <Route path="/login" exact component={Login} />
-                <Route path="/join" exact component={Join} />
-                <Route component={Error} />
-            </Switch>
+            {props.initialized ? (
+                <>
+                    <Header />
+
+                    <Switch>
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/join" exact component={Join} />
+                        <Route component={Error} />
+                    </Switch>
+                </>
+            ) : (
+                <LinearProgress />
+            )}
         </>
     );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+    initialized: state.app.initialized,
+    token: state.auth.token,
+});
+
+export default connect(mapStateToProps, { initializeApp })(App);
