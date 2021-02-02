971d3d79d22018cd7d2bcf9f241ff4209b4e50e6
Dmitry Dutin <dmitrydutin@gmail.com>
2021-02-02 19:01:26 +0300

    fix: edit route nmae

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,7 @@ import sequelize from './database/main';
 
 import authRouter from './routes/auth';
 import feedRouter from './routes/feed';
-import chatRouter from './routes/chat';
+import messagesRouter from './routes/messages';
 import dialogsRouter from './routes/dialogs';
 import exampleRouter from './routes/example';
 
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 
 app.use('/api/auth', authRouter);
 app.use('/api/feed', feedRouter);
-app.use('/api/chat', chatRouter);
+app.use('/api/messages', messagesRouter);
 app.use('/api/dialogs', dialogsRouter);
 app.use('/api/example', exampleRouter);
 
